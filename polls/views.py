34bca217a795d47835b7f7f0b7d806b720657190
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2023-11-27 22:46:46 -0600

    Updates to polls app

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
 
-def index(request):  # pylint: disable=unused-argument
+def index(_):  # pylint: disable=unused-argument
     return HttpResponse("Hello! Polls index")
+
+
+def detail(_, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(_, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(_, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
